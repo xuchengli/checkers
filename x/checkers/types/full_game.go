6bc8af22120d1a1555be0c57e2a454942676a29c
lixucheng <lixucheng@aliyun.com>
2023-01-30 11:09:43 +0800

    update helper function

--- x/checkers/types/full_game.go
+++ x/checkers/types/full_game.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/b9lab/checkers/x/checkers/rules"
+	"github.com/xuchengli/checkers/x/checkers/rules"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
